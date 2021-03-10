e469f9093372b5dafea61f4eaba645a4c579e492
mgaleste <mgaleste@gmail.com>
2021-03-10 15:58:19 +0800

    node sql

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const bodyParser = require('body-parser');
 //const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// db.execute('SELECT * FROM products')
+//     .then((result) => {
+//         console.log(result[0],result[1])
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
 
 const app = express();
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -6,38 +6,54 @@ exports.getProducts = (req, res, next)=> {
     //console.log('shop.js',adminData.products);
     //res.sendFile(path.join(rootDir,'views','shop.html'));
     //const products =  adminData.products;
-    const products = Product.fetchAll((products)=> {
+    // const products = Product.fetchAll((products)=> {
+    //     res.render('shop/product-list',{
+    //         prods: products, 
+    //         pageTitle: 'All Products', 
+    //         path: '/products', 
+    //         hasProducts: products.length > 0, 
+    //         activeShop: true
+    //     });
+    // });
+    Product.fetchAll()
+    .then(([rows,fieldData]) => {
         res.render('shop/product-list',{
-            prods: products, 
+            prods: rows, 
             pageTitle: 'All Products', 
-            path: '/products', 
-            hasProducts: products.length > 0, 
-            activeShop: true
+            path: '/products'
         });
-    });
+    })
+    .catch(err => console.log(err));
     
 };
 
 exports.getProduct = (req, res, next)=> {
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
+    Product.findById(prodId)
+    .then(([product])=>{
         res.render('shop/product-detail',{
-            product: product,
+            product: product[0],
             pageTitle : product.title,
             path: '/products'
         });
-    });
+    })
+    .catch(err => console.log(err));
+    
     
 };
 
 exports.getIndex = (req, res, next) => {
-    const products = Product.fetchAll((products)=> {
+    Product.fetchAll()
+    .then(([rows,fieldData]) => {
         res.render('shop/index',{
-            prods: products, 
+            prods: rows, 
             pageTitle: 'Shop', 
             path: '/'
         });
-    });
+    })
+    .catch(err => console.log(err));
+
+    
 };
 
 exports.getCart = (req, res, next) => {
