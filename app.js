0d57ba488fb864a9baa9f2ea06e3ccb3f971f206
mgaleste <mgaleste@gmail.com>
2021-03-11 08:29:24 +0800

    sequelize sync

--- app.js
+++ app.js
@@ -4,27 +4,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const expressHbs = require('express-handlebars');
+
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// db.execute('SELECT * FROM products')
-//     .then((result) => {
-//         console.log(result[0],result[1])
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     });
+
 
 const app = express();
 
-//app.engine('hbs', expressHbs({layoutsDir: 'views/layout/', defaultLayout: 'main-layout', extname: 'hbs'}));
-//app.set('view engine','hbs');
-//app.set('view engine','pug');
+
 app.set('view engine','ejs');
 app.set('views','views');
 
@@ -34,12 +26,16 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-//app.use((req,res,next) => {
-//    res.render('404',{pageTitle : 'Page Not Found'});
-    //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 
-//});
 app.use(errorController.get404);
-//const server = http.createServer(app);
 
-app.listen(3000);
+
+sequelize.sync()
+    .then(result=>{
+        //console.log(result);
+        app.listen(3000);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+
